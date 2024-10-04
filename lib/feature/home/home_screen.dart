0034afd592e87df950ad20b9db6124f779e12b91
igmerwina <erwin.ardiantha@pegadaian.co.id>
2024-10-04 10:43:21 +0700

    [UPD] - add new comer detail

--- lib/feature/home/home_screen.dart
+++ lib/feature/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:doctor_booking_app/data/data.dart';
 import 'package:doctor_booking_app/feature/home/biduan_detail.dart';
+import 'package:doctor_booking_app/feature/home/new_comer_detail.dart';
 import 'package:doctor_booking_app/model/speciality.dart';
 import 'package:doctor_booking_app/views/doctor_info.dart';
 import 'package:flutter/material.dart';
@@ -132,7 +133,22 @@ class _HomeScreenState extends State<HomeScreen> {
               SizedBox(
                 height: 20,
               ),
-              BiduanTile()
+              BiduanTile(),
+
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Top New Comer",
+                style: TextStyle(
+                    color: Colors.black87.withOpacity(0.8),
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              NewComer()
             ],
           ),
         ),
@@ -327,19 +343,6 @@ class BiduanTile extends StatelessWidget {
               ],
             ),
             Spacer(),
-            // Container(
-            //   padding: EdgeInsets.symmetric(horizontal: 15, vertical: 9),
-            //   decoration: BoxDecoration(
-            //       color: Color(0xffFBB97C),
-            //       borderRadius: BorderRadius.circular(13)),
-            //   child: Text(
-            //     "Call",
-            //     style: TextStyle(
-            //         color: Colors.white,
-            //         fontSize: 13,
-            //         fontWeight: FontWeight.w500),
-            //   ),
-            // ) // For iOS
 
             ElevatedButton(
               style: ElevatedButton.styleFrom(
@@ -365,6 +368,89 @@ class BiduanTile extends StatelessWidget {
                   // Android: Use FlutterPhoneDirectCaller to make the call directly
                   bool? res =
                       await FlutterPhoneDirectCaller.callNumber(phoneNumber);
+                  if (res != null && res) {
+                    print('Call made successfully');
+                  } else {
+                    print('Call failed');
+                  }
+                }
+              },
+              child: Text(
+                "Call",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewComer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => NewComerDetail()));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: Color(0xffFFEEE0), borderRadius: BorderRadius.circular(20)),
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+        child: Row(
+          children: <Widget>[
+            Image.asset(
+              "assets/45.png",
+              height: 50,
+            ),
+            SizedBox(
+              width: 17,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Inul Otista",
+                  style: TextStyle(color: Color(0xffFC9535), fontSize: 19),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Text(
+                  "3 Bintang",
+                  style: TextStyle(fontSize: 15),
+                )
+              ],
+            ),
+            Spacer(),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xffFBB97C),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(13)),
+              ),
+              onPressed: () async {
+                const phoneNumber = '085695001647';
+
+                if (Theme.of(context).platform == TargetPlatform.iOS) {
+                  final Uri launchUri =
+                  Uri.parse('https://wa.me/6285695001647');
+
+                  if (await canLaunchUrl(launchUri)) {
+                    await launchUrl(launchUri);
+                  } else {
+                    print('Could not launch dialer');
+                  }
+                } else {
+                  // Android: Use FlutterPhoneDirectCaller to make the call directly
+                  bool? res =
+                  await FlutterPhoneDirectCaller.callNumber(phoneNumber);
                   if (res != null && res) {
                     print('Call made successfully');
                   } else {
---
+++ lib/feature/home/new_comer_detail.dart
@@ -0,0 +1,288 @@
+import 'package:doctor_booking_app/feature/schedule/schedule.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class NewComerDetail extends StatefulWidget {
+  @override
+  _NewComerDetailState createState() => _NewComerDetailState();
+}
+
+class _NewComerDetailState extends State<NewComerDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          systemNavigationBarColor: Colors.blue, // Navigation bar
+          statusBarColor: Colors.red, // Status bar
+        ),
+        iconTheme: IconThemeData(color: Colors.black87),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(child: Image.asset("assets/45.png", height: 220),),
+
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 222,
+                    height: 220,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Inul Otista",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                        Text(
+                          "Goyang Dumang Speailist",
+                          style: TextStyle(fontSize: 19, color: Colors.grey),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            IconTile(
+                              backColor: Color(0xffFFECDD),
+                              imgAssetPath: "assets/email.png",
+                            ),
+                            IconTile(
+                              backColor: Color(0xffFEF2F0),
+                              imgAssetPath: "assets/call.png",
+                            ),
+                            IconTile(
+                              backColor: Color(0xffEBECEF),
+                              imgAssetPath: "assets/video_call.png",
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 26,
+              ),
+              Text(
+                "About",
+                style: TextStyle(fontSize: 22),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                  "Inul Otista, yang memiliki nama asli Inul Otista Raya, adalah seorang penyanyi dangdut terkenal asal Indonesia yang lahir pada 21 Januari 1779 di Pasuruan, Jawa Timur. Ayo Booking Saya bapak-bapak",
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+                textAlign: TextAlign.justify,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Image.asset("assets/mappin.png"),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "Address",
+                                style: TextStyle(
+                                    color: Colors.black87.withOpacity(0.7),
+                                    fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Container(
+                                  width:
+                                  MediaQuery.of(context).size.width - 268,
+                                  child: Text(
+                                    "Jl. Embong Malang No. 1,3,5, Tunjungan Plaza Tunjungan Plaza 5, Lantai 6, Surabaya Telp. (031) 546 8282",
+                                    style: TextStyle(color: Colors.grey),
+                                  ))
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Image.asset("assets/clock.png"),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "Daily Practice",
+                                style: TextStyle(
+                                    color: Colors.black87.withOpacity(0.7),
+                                    fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Container(
+                                  width:
+                                  MediaQuery.of(context).size.width - 268,
+                                  child: Text(
+                                    '''Monday - Friday
+Open till Drop''',
+                                    style: TextStyle(color: Colors.grey),
+                                  ))
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  Image.asset(
+                    "assets/map.png",
+                    width: 180,
+                  )
+                ],
+              ),
+              Text(
+                "Activity",
+                style: TextStyle(
+                    color: Color(0xff242424),
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 22,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      padding:
+                      EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+                      decoration: BoxDecoration(
+                          color: Color(0xffFBB97C),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                  color: Color(0xffFCCA9B),
+                                  borderRadius: BorderRadius.circular(16)),
+                              child: Image.asset("assets/list.png")),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width / 2 - 130,
+                            child: Text(
+                              "Booking Now",
+                              style:
+                              TextStyle(color: Colors.white, fontSize: 17),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        // Navigate to a new screen
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ScheduleScreen(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding:
+                        EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+                        decoration: BoxDecoration(
+                            color: Color(0xffA5A5A5),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                                padding: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                    color: Color(0xffBBBBBB),
+                                    borderRadius: BorderRadius.circular(16)),
+                                child: Image.asset("assets/list.png")),
+                            SizedBox(
+                              width: 16,
+                            ),
+                            Container(
+                              width:
+                              MediaQuery.of(context).size.width / 2 - 130,
+                              child: Text(
+                                "Jadwal Harian",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class IconTile extends StatelessWidget {
+  final String? imgAssetPath;
+  final Color? backColor;
+
+  IconTile({this.imgAssetPath, this.backColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 16),
+      child: Container(
+        height: 45,
+        width: 45,
+        decoration: BoxDecoration(
+            color: backColor, borderRadius: BorderRadius.circular(15)),
+        child: Image.asset(
+          imgAssetPath!,
+          width: 20,
+        ),
+      ),
+    );
+  }
+}
